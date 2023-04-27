fb044d1252de446b773996248b487efab9a1fd19
xsahxl <xsahxl@126.com>
2023-04-27 19:30:18 +0800

    debug: onFinish
    Signed-off-by: xsahxl <xsahxl@126.com>

--- a.js
+++ a.js
@@ -1,8 +1,32 @@
 const { command } = require("execa");
 const path = require("path");
 
-command("mvn package -DskipTests", {
-  cwd: path.resolve(__dirname, "./code"),
-  shell: true,
-  stdio: "inherit",
-});
+function onFinish(cp) {
+  return new Promise((resolve, reject) => {
+    const stdout = [];
+    const stderr = [];
+    cp.stdout.on("data", (chunk) => {
+      console.info(chunk.toString());
+      stdout.push(chunk);
+    });
+
+    cp.stderr.on("data", (chunk) => {
+      console.info(chunk.toString());
+      stderr.push(chunk);
+    });
+
+    cp.on("exit", (code) => {
+      code === 0
+        ? resolve({})
+        : reject(new Error(Buffer.concat(stderr).toString()));
+    });
+  });
+}
+
+(async () => {
+  const cp = command("mvn package -DskipTests", {
+    cwd: path.resolve(__dirname, "./code"),
+    shell: true,
+  });
+  await onFinish(cp);
+})();
